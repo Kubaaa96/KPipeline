434424d5b0083245aee7b0e9c4a1e08c769ff28e
Jakub Dobrowolski <jd.kuba96@gmail.com>
2025-07-05 14:23:42 +0200

    Initial setup after using template

--- cmake/Helpers.cmake
+++ cmake/Helpers.cmake
@@ -1,7 +1,7 @@
 function(package_add_test TESTNAME)
     cmake_parse_arguments(ARGS "${options}" "" "SOURCES;DEPENDS" ${ARGN})
     project_options(
-            PREFIX "cpptemplatetest"
+            PREFIX "kpipelinetest"
             ENABLE_CACHE
             ${ENABLE_CPPCHECK}
             ${ENABLE_CLANG_TIDY}
@@ -9,7 +9,7 @@ function(package_add_test TESTNAME)
     )
     add_executable(${TESTNAME} ${ARGS_SOURCES})
     target_link_libraries(${TESTNAME}
-            PRIVATE cpptemplatetest_project_options cpptemplatetest_project_warnings
+            PRIVATE kpipelinetest_project_options kpipelinetest_project_warnings
     )
     target_include_directories(${TESTNAME} PUBLIC ${CMAKE_SOURCE_DIR}/include)
     find_and_link_libs(${TESTNAME})
